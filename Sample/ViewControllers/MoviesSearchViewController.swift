558706b587660693e0ec7ab0ef1af5a89559332b
emadgnia <emadgnia@gmail.com>
2020-09-14 07:27:17 +0200

    remove loading

--- Sample/ViewControllers/MoviesSearchViewController.swift
+++ Sample/ViewControllers/MoviesSearchViewController.swift
@@ -25,7 +25,6 @@ class MoviesSearchViewController : UIViewController {
         tableView.delegate = self
         return tableView
     }()
-    private lazy var alertViewController = AlertViewController()
     private lazy var searchController: UISearchController = {
         let searchController = UISearchController(searchResultsController: nil)
         searchController.obscuresBackgroundDuringPresentation = false
@@ -98,11 +97,9 @@ class MoviesSearchViewController : UIViewController {
             loadingView.startAnimating()
             update(with: [], animate: true)
         case .noResults:
-            alertViewController.showNoResults(sender: self)
             loadingView.stopAnimating()
             update(with: [], animate: true)
         case .failure:
-            alertViewController.showDataLoadingError(sender: self)
             loadingView.stopAnimating()
             update(with: [], animate: true)
         case .success(let movies):
